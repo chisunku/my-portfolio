7c920d846ffb797ab51e011d0c05a42dd03c5e8e
chinmayi sunku <chinmayi.sunku@sjsu.edu>
2024-02-20 11:53:18 -0800

    resume link changed

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -8,7 +8,7 @@ const HeaderSocials = () => {
     <div className="header__socials">
         <a href="https://www.linkedin.com/in/chinmayi-sunku/" target="_blank" rel='noreferrer'><BsLinkedin/></a>
         <a href="https://github.com/chisunku" target="_blank" rel='noreferrer'><BsGithub/></a>
-        <a href="https://drive.google.com/file/d/17DDqYZu3FZ6CGqJSVbVUruW-EkmD-Nan/view?usp=sharing" target="_blank" rel='noreferrer'><ImProfile/></a>
+        <a href="https://drive.google.com/file/d/1zqeR16mdQSDyrRuT8plgC5FrhqgNlu68/view?usp=sharing" target="_blank" rel='noreferrer'><ImProfile/></a>
     </div>
   )
 }
